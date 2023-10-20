86127d2a7633065628ba11e8adbe4a7301ce35ff
Chukwuma Obasi <ccobasi8@gmail.com>
2023-10-20 14:11:05 +0100

    create serializers

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from .models import *
 from rest_framework import generics
-
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.views import APIView
 
 class Quiz(generics.ListAPIView):
-    
+    serializer_class = QuizSerializer
+    queryset = Quizzes.objects.all()
