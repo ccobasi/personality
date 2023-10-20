65b608d446b2f2c0a5b8838504ab4451079263f5
Chukwuma Obasi <ccobasi8@gmail.com>
2023-10-20 12:07:05 +0100

    Register models in admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+
+@admin.register(Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+    ]
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = [
+
+        'answer_text',
+        'is_introvert',
+    ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'quiz',
+    ]
+    list_display = [
+        'title',
+        'quiz',
+        
+    ]
+    inlines = [
+        AnswerInlineModel,
+    ]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'answer_text',
+        'is_introvert',
+        'question'
+    ]
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = [
+        'quiz',
+        'user',
+        'score'
+    ]
+
