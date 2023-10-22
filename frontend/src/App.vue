53138b1e9fdca8395ade25feab84e14f7d0e6d1e
Chukwuma Obasi <ccobasi8@gmail.com>
2023-10-22 01:34:33 +0100

    Create frontend vue vite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,24 @@
+<script setup>
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+
+  <body>
+
+  </body>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background: #271c36;
+  color: #fff;
+  height: 100vh;
 }
 </style>
