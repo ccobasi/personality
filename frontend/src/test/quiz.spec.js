90f0a86b2a13cd102af5e6c4049ba5e655ffc13b
Chukwuma Obasi <ccobasi8@gmail.com>
2023-11-18 23:49:55 +0100

    create jest configuration

--- frontend/src/test/quiz.spec.js
+++ frontend/src/test/quiz.spec.js
@@ -1,7 +1,25 @@
-import { mount } from '@vue/test-utils'
-import App from '@/App'
-import { describe } from 'node:test'
+import { mount } from '@vue/test-utils';
+import QuizComponent from './QuizComponent.vue';
 
-describe('App.vue', () => {
-    it('Renders')
-})
+describe('QuizComponent', () => {
+  it('renders correctly', async () => {
+    const wrapper = mount(QuizComponent);
+
+    // Add your test assertions here
+
+    // For example:
+    expect(wrapper.find('h1').text()).toBe('Quizzes');
+  });
+
+  it('emits correct events when interacting with the component', async () => {
+    const wrapper = mount(QuizComponent);
+
+    // Simulate user interactions and check if the expected events are emitted
+
+    // For example:
+    await wrapper.find('input[type="radio"]').trigger('change');
+    await wrapper.find('button').trigger('click');
+
+    // Add your test assertions here
+  });
+});
