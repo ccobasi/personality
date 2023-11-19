f20c6f94e6a67dbed7f893383c5c2ba0abc0f5ce
Chukwuma Obasi <ccobasi8@gmail.com>
2023-11-19 14:31:06 +0100

    Update test

--- frontend/src/test/quiz.spec.js
+++ frontend/src/test/quiz.spec.js
@@ -1,38 +1,32 @@
 import { mount } from '@vue/test-utils';
 import QuizView from '../components/QuizView.vue';
 
-// describe('QuizView', () => {
-//   it('renders correctly', async () => {
-//     const wrapper = mount(QuizView);
-
-   
-//     expect(wrapper.find('h1').text()).toBe('Quizzes');
-//   });
-
-//   it('emits correct events when interacting with the component', async () => {
-//     const wrapper = mount(QuizView);
-
-    
-//     await wrapper.find('input[type="radio"]').trigger('change');
-//     await wrapper.find('button').trigger('click');
-//     console.log(wrapper.html())
-
-    
-//   });
-// });
-
 describe('QuizView', () => {
   it('emits correct events when interacting with the component', async () => {
     const wrapper = mount(QuizView);
 
     expect(wrapper.exists()).toBe(true);
     
-  
-   
-
-    
   });
 
+  it('checks if quiz is not completed', async () => {
+    const quizCompleted = false;
+
+    const wrapper = mount(QuizView, {
+      data() {
+        return {
+          quizCompleted,
+        };
+      },
+    });
+    await wrapper.vm.$nextTick();
+
+    if (quizCompleted) {
+      expect(wrapper.text()).toContain('You have finished the quiz!');
+    } else {
+      expect(wrapper.text()).toContain('Next Question');
+    }
+  })
 })
 
 
